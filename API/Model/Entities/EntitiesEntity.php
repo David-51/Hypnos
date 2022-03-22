cb8567031e8db1c00960481346fdd701863dcbea
David GRIGNON <david@davidgrignon.com>
2022-03-22 01:03:50 +0100

    UPDATE setUnidId with Random Ip if no remote adress detected

--- API/Model/Entities/EntitiesEntity.php
+++ API/Model/Entities/EntitiesEntity.php
@@ -29,19 +29,26 @@ class Entities
             $this->$key = $value;
         }               
     }    
-    // UniqId is based on microsecond timestamp and the client's remote Ip, the last is random hexadecimal
+    // UniqId is based on microsecond timestamp and the client's remote Ip, the last is random hexadecimal. If no Ip is detected, random Ip
     public function setUniqId(){
         
         function ipToHex(){
             $remote = $_SERVER['REMOTE_ADDR'];
             $explode_remote = explode('.', $remote);
-            $map = array_map(function($value) {
-                if($value<16){
-                    return '0'.dechex(intval($value));
+            
+            for($i = 0; $i<4 ; $i++){
+
+                if(!isset($explode_remote[$i])){
+                    $explode_remote[$i] = rand(0,255);
                 }
-                return dechex(intval($value));
-            }, $explode_remote);
-            return implode('', $map);
+                if(intval($explode_remote[$i])<16){
+                    $ip[] = '0'.dechex(intval($explode_remote[$i]));
+                }
+                else{
+                    $ip[] = dechex(intval($explode_remote[$i]));
+                }
+            }            
+            return implode('', $ip);
         }        
         $uniqid = uniqid(true);    
         return substr($uniqid, 0, 8). '-' . substr($uniqid, 8, 4) . '-' . substr($uniqid, 12, 2). bin2hex(random_bytes(1)) . '-' . bin2hex(random_bytes(2)) . '-' . bin2hex(random_bytes(2)) . ipToHex();
