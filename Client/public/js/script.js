6f6830616b97394d1cfc1c6b4bba6f631070763c
root <root@localhost.localdomain>
2022-04-10 20:42:22 +0000

    Revert "FIX screen Mobile Bug on vh"
    This reverts commit 60287ff20223ea72bb84586efb3f0b4a347f2c60.

--- Client/public/js/script.js
+++ Client/public/js/script.js
@@ -1,7 +1,6 @@
 let hamburgerMenu = document.getElementById('hamburgerMenu');
 let shutter = document.getElementById('shutter');
 
-
 hamburgerMenu.addEventListener('click', () => {
     shutter.classList.toggle('displaybar');
 })
@@ -9,9 +8,3 @@ hamburgerMenu.addEventListener('click', () => {
 shutter.addEventListener('click', () => {
     shutter.classList.toggle('displaybar');
 })
-
-// getting a vh unit from the 100% windows
-let vh = window.innerHeight * 0.01;
-
-const mainContent = document.getElementById('mainContent');
-mainContent.style.setProperty('--vh', `${vh}px`);
