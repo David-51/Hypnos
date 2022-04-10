da7ebbab69d00a3f285c7fda621dc3782e306a13
David GRIGNON <david@davidgrignon.com>
2022-04-10 22:45:09 +0200

    FIx

---
+++ API/Model/Manager/DatabaseManager.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace API\Model\Manager;
+
+class Database
+{
+
+    const DSN = 'mysql:dbname=hypnos;host:127.0.0.1;charset=UTF8';
+    // const USERNAME = 'administrator';
+    // const PASSWORD = 'administrator';
+    const USERNAME = 'admin';
+    const PASSWORD = 'admin';
+    
+    
+    public static $pdo = null;
+    
+    private static function connect() :\PDO
+    {
+        if(!(self::$pdo)){                        
+            try{
+                self::$pdo = new \PDO(self::DSN, self::USERNAME, self::PASSWORD);                
+                self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            }
+            catch(\PDOException $e){
+                http_response_code(404);
+                echo "Error de connexion <br>". $e;
+            }
+            return self::$pdo;
+        }
+        else {            
+            // echo "connecté";
+            return self::$pdo;
+        }
+    }
+
+    public static function getConnection() :\PDO
+    {
+        return self::connect();
+    }
+}
